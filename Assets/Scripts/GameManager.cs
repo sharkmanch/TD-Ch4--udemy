4bdd0aab91f62a63d18830fb7295d6e0e9da510b
sharkmanch <danteman1992@gmail.com>
2018-01-04 00:51:41 +0800

    map settled, gameManager and loader settled, the game now spawn enemie[0] onto spawnPoint at start

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public static GameManager instance = null;
+    //creating the one single instance of game manager 
+
+
+    public GameObject spawnPoint;
+    
+
+    public GameObject[] enemies;
+    public int maxEnemiesOnScreen;
+    public int totalEnemies;
+    public int enemiesPerSpawn;
+
+    private int enemiesOnScreen = 0;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        spawnEnemy();
+	}
+
+    void spawnEnemy()
+    {
+        if(enemiesPerSpawn > 0 && enemiesOnScreen < totalEnemies)
+        {
+            for(int i = 0; i < enemiesPerSpawn; i++)
+            {
+                if (enemiesOnScreen < maxEnemiesOnScreen)
+                {
+                    GameObject newEnemy = Instantiate(enemies[0]) as GameObject;
+                    newEnemy.transform.position = spawnPoint.transform.position;
+                    enemiesOnScreen += 1;
+                }
+            }
+        }
+
+    }
+
+
+}
