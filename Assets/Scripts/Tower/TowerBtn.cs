84d09c6f42a8062d65142b07ee4d40dd236691d0
sharkmanch <danteman1992@gmail.com>
2018-01-18 23:41:44 +0800

    added buttons, set singletons of towerManager and prefabs, now moving onto build tower. Bug: DontDestroyOnLoad stopping spawned enemies to destroy itself

---
+++ Assets/Scripts/Tower/TowerBtn.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class TowerBtn : MonoBehaviour {
+
+    // Use this for initialization
+    [SerializeField]
+    private GameObject towerObject;
+
+    public GameObject TowerObject
+    {
+        get { return towerObject; }//getter to get the button set here.
+    }
+
+	}
