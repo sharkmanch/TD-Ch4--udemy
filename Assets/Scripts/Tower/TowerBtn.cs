93686d5036f734b507111c9ddcb4a855a11e682b
sharkmanch <danteman1992@gmail.com>
2018-01-28 01:53:35 +0800

    added projectilescript, set enum for projectiles

--- Assets/Scripts/Tower/TowerBtn.cs
+++ Assets/Scripts/Tower/TowerBtn.cs
@@ -5,10 +5,18 @@ public class TowerBtn : MonoBehaviour {
     // Use this for initialization
     [SerializeField]
     private GameObject towerObject;
-
+    [SerializeField]
+    private Sprite dragSprite;
     public GameObject TowerObject
     {
         get { return towerObject; }//getter to get the button set here.
+       
     }
 
-	}
+    public Sprite DragSprite
+    {
+        get { return dragSprite; }//getter to get the button set here.
+
+    }
+
+}
--- Assets/Scripts/TowerManager.cs
+++ Assets/Scripts/TowerManager.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 public class TowerManager : Singleton<TowerManager> {
 
     private TowerBtn towerBtnPressed;
-
+    private SpriteRenderer spriteRenderer;
 
 
 
 	// Use this for initialization
 	void Start () {
-		
+        spriteRenderer = GetComponent<SpriteRenderer>(); //looking for the component in unity, it grab the ref of the renderer to this script. we need to have a sprite attached to the mouse, so that the sprite can follow the mouse (to remind you what tower you are dragging onto buildsite.)
 	}
 	
 	// Update is called once per frame
@@ -21,12 +21,17 @@ public class TowerManager : Singleton<TowerManager> {
             RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
             if (hit.collider.tag == "buildSite")
             {
-                hit.collider.tag = "buildSiteFull";
+                hit.collider.tag = "buildSiteFull"; //no == coz we are setting it not if.
                 placeTower(hit);
             }
-
+         
         }
-	}
+
+        if (spriteRenderer.enabled)
+        {
+            followMouse();
+        }//need 3 methods to tell sprite to follow the mouse, enable the sprite renderer, then we set the sprite render to follow the mouse, and disable it when dropped off.
+    }
 
     public void placeTower(RaycastHit2D hit)
     {
@@ -34,6 +39,7 @@ public class TowerManager : Singleton<TowerManager> {
         {
             GameObject newTower = Instantiate(towerBtnPressed.TowerObject);
             newTower.transform.position = hit.transform.position;
+            disableDragSprite();
         }
     }
 
@@ -41,12 +47,28 @@ public class TowerManager : Singleton<TowerManager> {
     public void selectedTower(TowerBtn towerSelected)
     {
         towerBtnPressed = towerSelected;
+        enableDragSprite(towerBtnPressed.DragSprite);
     //    Debug.Log("pressed!!! " + towerBtnPressed.gameObject);
 
     }
 
+    public void followMouse()
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector2(transform.position.x, transform.position.y);
+    }
 
+    public void enableDragSprite(Sprite sprite)
+    {
+        spriteRenderer.enabled = true;
+        spriteRenderer.sprite = sprite;
+    }
 
+    public void disableDragSprite()
+    {
+        spriteRenderer.enabled = false;
+
+    }
 
 
 }
